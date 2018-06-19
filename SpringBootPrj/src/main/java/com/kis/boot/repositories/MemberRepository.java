1271baaf5b02a959736d6f7fea16b1cc0f021c5a
iskwon <kis6905@gmail.com>
2018-06-19 16:19:05 +0900

    [Remove] SpringBootPrj(legacy)

--- SpringBootPrj/src/main/java/com/kis/boot/repositories/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kis.boot.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kis.boot.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
